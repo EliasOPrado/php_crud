10a3d2ed9beb522526adaa26b83163d1ece23b49
EliasOPrado <eliaspradoprofessional@outlook.com>
2020-04-12 07:43:47 +0100

    added update functionality

--- process.php
+++ process.php
@@ -3,6 +3,11 @@ session_start();
 // empty fild means no password
 $mysqli = new mysqli('localhost', 'root', '', 'crud') or die(mysqli_error($mysqli));
 
+$id = 0;
+$name = "";
+$location = "";
+$update = false;
+
 if(isset($_POST['save'])){
   $name = $_POST['name'];
   $location = $_POST['location'];
@@ -22,6 +27,29 @@ if(isset($_GET['delete'])){
   $_SESSION['message'] = "Record has been deleted!";
   $_SESSION['msg_type'] = "danger";
 
-  header("header: index.php");
+header("location: index.php");
+}
+
+if(isset($_GET['edit'])){
+  $id = $_GET['edit'];
+  $update = true;
+  $result = $mysqli->query("SELECT * FROM data WHERE id=$id") or die($mysqli->error());
+  if (count($result)==1){
+    $row = $result->fetch_array();
+    $name = $row['name'];
+    $location = $row['location'];
+  }
+}
+
+if(isset($_POST['update'])){
+  $id = $_POST['id'];
+  $name = $_POST['name'];
+  $location = $_POST['location'];
+
+  $mysqli->query("UPDATE data SET name='$name', location='$location' WHERE id='$id'") or die($mysqli->error);
+  $_SESSION['message'] = "Record has been updated!";
+  $_SESSION['msg_type'] = "warning";
+
+  header("location: index.php");
 }
 ?>
